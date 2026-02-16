5f35d35699803de723fd926564b09a28639b1158
54raymondwang <54raymondwang@gmail.com>
2026-02-16 21:58:47 +0800

    Add Python Hello World script

---
+++ hello_world.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Simple Hello World program in Python
+Created via Antigravity
+"""
+
+def main():
+    print("Hello, World!")
+    print("Welcome to go-antig-test repository!")
+    print("This file was created using Antigravity 🚀")
+
+if __name__ == "__main__":
+    main()
